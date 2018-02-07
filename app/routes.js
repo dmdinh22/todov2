9c9aed8ed638ba5a2ba2f751f826e1edd0974464
David Dinh <dmdinh22@gmail.com>
2018-02-06 20:39:37 -0800

    changed index route

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,4 @@
+// load the todo model
 var Todo = require('./models/todo');
 
 function getTodos(res) {
@@ -52,6 +53,6 @@ module.exports = function (app) {
 
     // application -------------------------------------------------------------
     app.get('*', function (req, res) {
-        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendFile(__dirname + './public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
 };
